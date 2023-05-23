b7656c844064974157d95e40ccd54f1ebccdc41d
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-23 21:03:00 +0300

    function to add node to the stack

---
+++ node_add.c
@@ -0,0 +1,27 @@
+#inxlude "monty.h"
+
+/**
+ * node_add - adds a node to the head stack
+ * @head: the head to the stack
+ * @num: new integer
+ * Return: nothing
+ */
+
+void node_add(stack_t **head, int num)
+{
+	stack_t *new_node, *stk;
+
+	stk = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+	if (stk)
+		stk->prev = new_node;
+	new_node->num = num;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+}
